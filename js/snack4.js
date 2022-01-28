a7e340f1444ecd0333dbf85ead56b2039629671e
jackbottaro <mail.giacomobottaro@gmail.com>
2022-01-28 15:59:34 +0100

    snack4 done

---
+++ js/snack4.js
@@ -0,0 +1,35 @@
+/*
+Crea un array di numeri interi
+e fai la somma di tutti gli elementi che sono in posizione dispari
+*/
+
+// Creo Array
+const number = [11, 22, 130, 90, 3, 8, 12, 77]
+
+// Dichiaro variabile di appoggio
+let sum = 0;
+
+// creo ciclo per fare la somma dei numeri dispari
+for(let i = 0; i < number.length; i = i +2){
+    if(!(number[i] % 2 == 0)){
+        sum += number[i]
+    }
+
+}
+
+console.log(sum)
+
+// Creo ciclo con while
+
+let sum = 0;
+let i = 0;
+
+while(i < number.length){
+    if(!(number[i] % 2 == 0)){
+        sum += number[i]
+    }
+    i++
+}
+
+
+console.log(sum)
